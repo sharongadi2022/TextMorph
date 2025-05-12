ac04dfe2c10e62b3dd4d4f9a10a5372be3980089
sharongadi2022 <gadisharonhepsibah2022@vitbhopal.ac.in>
2025-05-12 13:11:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='IntelliDocs',
+    name='TextMorph',
     version='1.0.0',
     packages=[''],
     install_requires=[required],
     url='',
     license='',
-    author='IntelliDocs',
-    author_email='rishixc@gmail.com'
+    author='TextMorph',
+    author_email='sharongadi2004@gmail.com'
     description=''
 )
